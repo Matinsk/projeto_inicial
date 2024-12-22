658ddaa014dc9bf382f7d3df62f93dc852ebf626
matheus.antonio <matheusk42@hotmail.com>
2024-12-22 13:17:23 -0300

    Meu primeiro commit

---
+++ app/main.py
@@ -0,0 +1,18 @@
+idade = 32
+nome = "Luciano"
+
+print(idade)
+
+nova_idade = idade + 1
+
+print(nova_idade)
+
+nova_idade_nova = nova_idade + 1
+
+print(nova_idade_nova)
+
+list = [1,2,3,4,5]
+
+lista_invertida = list.reverse()
+
+print(lista_invertida)
